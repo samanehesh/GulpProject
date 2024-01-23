94c7d88bc4bdc45bac78b24bb158d491d637f387
thuang79 <thuang79@my.bcit.ca>
2024-01-23 12:42:01 -0800

    partials for header, nav, hamburger

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,28 @@
 // Write your JavaScript here...
+const body = document.body;
+const btnMenu = document.getElementById('btn-menu');
+const nav = document.getElementById('main-navigation');
+
+btnMenu.addEventListener('click', openMenu);
+btnMenu.addEventListener('mousedown', function(e){
+    e.preventDefault();
+});
+
+function openMenu(){
+    body.classList.toggle('show');
+	nav.classList.add('activated');
+}
+
+const mql = window.matchMedia('(min-width: 500px)');
+
+
+mql.addListener(removeTransition);
+
+
+function removeTransition(e){
+
+	if(e.matches){
+		body.classList.remove('show');
+		nav.classList.remove('activated');
+	}
+}
